7c6c2fa5b7458bc8c5544ac23be572e1d2e0b7ff
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-05-17 13:46:46 +0100

    commit

--- hangMan.py
+++ hangMan.py
@@ -1,4 +1,5 @@
 import time
+import random
 
 HANGMANPICS = ['''
   +---+
@@ -50,6 +51,13 @@ HANGMANPICS = ['''
  / \  |
       |
 =========''']
+words = ('ant baboon badger bat bear beaver camel cat clam cobra cougar '
+         'coyote crow deer dog donkey duck eagle ferret fox frog goat '
+         'goose hawk lion lizard llama mole monkey moose mouse mule newt '
+         'otter owl panda parrot pigeon python rabbit ram rat raven '
+         'rhino salmon seal shark sheep skunk sloth snake spider '
+         'stork swan tiger toad trout turkey turtle weasel whale wolf '
+         'wombat zebra ').split()
 print("welcome to Hangman")
 time.sleep(1.5)
 print("Guess the letters in a random word.\nIf your letter is actually a part of the word it is added to the spelling.")
@@ -59,4 +67,17 @@ time.sleep(2)
 print(f"If you guess a wrong letter 8 times hangman dies")
 time.sleep(2)
 print(f' {HANGMANPICS[6]}')
-Isplay = input("Enter 'start' if your'e ready to play: ")
+isplay = input("Enter 'start' if you're ready to play: ")
+isSure = 'N'
+if isplay.lower() == "start":
+    print("GAME ON!!!");
+else:
+    isSure = input("Enter the word 'start' if your'e willing to play\nIf not enter 'N' for no.")
+if isplay.lower == "start" or isSure.lower != 'n':
+    games = int(input("how many games do you want to play?: "))
+    for i in range(games):
+        word = random.choice(words)
+        length =  len(word)
+        print(f'Your word is a {length} letter word')
+        for d in range(length):
+            print('_ ',end ="")
