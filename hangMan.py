e4039e270333c2f2dbd34c60da8dbd437ed89cb9
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-05-17 13:10:24 +0100

    commit

---
+++ hangMan.py
@@ -0,0 +1,7 @@
+import time
+
+print("welcome to Hangman")
+time.sleep(1)
+print("Guess the letters in a random word.\nIf your letter is actually a part of the word it is added to the spelling.")
+time.sleep(1)
+print('If not. Hangman is slowly led towards death. If you guess the wrong letter multiple times hangman dies')
